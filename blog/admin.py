c8422135c2f7310061774b44cc6004ee8488ec5b
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-24 15:24:18 +0300

    register the class models to the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from blog.models import Project
+from blog.models import Project, Profile, Review, Rating
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Review)
+admin.site.register(Rating)
