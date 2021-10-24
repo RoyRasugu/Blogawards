6cd80f1eee3ad62a834b7f2327739b53a89bf879
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-24 19:08:07 +0300

    add the projectList and profileList api view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from blog.models import Project, Profile
+from .serializers import ProfileSerializer, ProjectSerializer
 
 # Create your views here.
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
+    serializer_class = ProfileSerializer
+
+class ProjectList(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = ProjectSerializer
