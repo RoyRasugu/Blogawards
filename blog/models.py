50b8a9db7883955d4773f58b4ccfccae483896b4
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-24 11:10:16 +0300

    create the model class project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    img_post = models.ImageField(upload_to="project/")
+    description = HTMLField()
+    project_url = models.URLField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    
