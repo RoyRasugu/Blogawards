dea522a6707c24032218cc0ff07103a64be2e4f1
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-30 09:15:58 +0300

    add the configuration settings for media

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from blog import views
+from blog.views import home, register, login, ProfileList, ProjectList
+from django.conf import settings
+from django.conf.urls.static import static
+
+from award_clone.settings import MEDIA_ROOT
 
 urlpatterns = [
-    path('api/profile/', views.ProfileList.as_view()),
-    path('api/project/', views.ProjectList.as_view()),
+    path('', home, name='home'),
+    path('signup', register, name='register'),
+    path('login', login, name='login'),
+    path('api/profile/', ProfileList.as_view()),
+    path('api/project/', ProjectList.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = MEDIA_ROOT)
