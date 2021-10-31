f3a904440dfa12c7c2e77e3b228c2119c305452b
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-31 09:10:12 +0300

    change url views for project api

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-from blog.views import home, register, login, ProfileList, ProjectList
+from blog.views import projectApi
 from django.conf import settings
 from django.conf.urls.static import static
 
 from award_clone.settings import MEDIA_ROOT
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('signup', register, name='register'),
-    path('login', login, name='login'),
-    path('api/profile/', ProfileList.as_view()),
-    path('api/project/', ProjectList.as_view()),
+    path('project/', projectApi),
+    path('project/([0-9]+)', projectApi),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = MEDIA_ROOT)
